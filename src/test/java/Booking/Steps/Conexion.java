3996670862738b68d169b973123367d070c70ff1
German <german.portilla.gonzalez@gmail.com>
2023-01-23 17:01:34 -0500

    Update proyecto clase automation

---
+++ src/test/java/Booking/Steps/Conexion.java
@@ -0,0 +1,30 @@
+package Booking.Steps;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.Before;
+import net.thucydides.core.annotations.Step;
+
+public class Conexion {
+
+	private WebDriver driver;
+
+	@Before
+	@Step
+	public WebDriver abrirNavegador() {
+		System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.navigate().to("https://www.booking.com/index.es.html");
+		
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		
+		return this.driver;
+
+	}
+	
+
+}
